e91ea5f138287d44d4d8e035d9dbc632e5d15b6b
hg_jeon <jeonhgfm101@gmail.com>
2019-09-18 13:11:30 +0900

    split map pow toFixed parseFloat 사용

--- second.js
+++ second.js
@@ -7,13 +7,20 @@ const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
-
 rl.on("line", function(line) {
-	
-	let valueArr = line.split(" ");
-	let totalMoney = valueArr[0]*Math.pow(1+(valueArr[1]/100), valueArr[2]);
-	console.log(totalMoney.toFixed(2));
-	
+    var strArray = line.split('_')
+    // for(var i=0; i<strArray.length; i++){
+    //     strArray[i] = strArray[i]*1
+    // }
+    // strArray.forEach(function(item){
+    //     item = item * 1
+    // })
+    var Array = strArray.map(function(str){
+        return Number(str)
+    })
+    //https://bblog.tistory.com/300 배열오소 일괄변경할때는 map쓰자
+    var yield = (Array[0]*Math.pow(1+Array[1]/100,Array[2])).toFixed(2)
+	console.log(strArray, Array, parseFloat(yield))	
 	rl.close();
 }).on("close", function() {
 	process.exit();
